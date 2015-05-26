ba29577400b7c9f18124b2088f9580fe245b9e37
OA Wu <comdan66@gmail.com>
2015-05-26 10:33:35 +0800

    Init code.

---
+++ cmd/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp       = require ('gulp'),
+    livereload = require('gulp-livereload'),
+    uglifyJS   = require ('gulp-uglify'),
+    htmlmin    = require('gulp-html-minifier'),
+    del        = require('del');
+
+// ===================================================
+
+gulp.task ('default', function () {
+
+  livereload.listen ();
+
+  ['./root/*.html', './root/css/**/*.css', './root/js/**/*.js'].forEach (function (t) {
+    gulp.watch (t).on ('change', function () {
+      gulp.run ('reload');
+    });
+  });
+});
+gulp.task ('reload', function () {
+  livereload.changed ();
+  console.info ('\nReLoad Browser!\n');
+});
+
+// ===================================================
+
+gulp.task ('minify', function () {
+  gulp.run ('js-uglify');
+  gulp.run ('res-uglify');
+  gulp.run ('minify-html');
+});
+gulp.task ('gh-pages', function () {
+  del (['./root']);
+});
+gulp.task ('js-uglify', function () {
+  gulp.src ('./root/js/**/*.js')
+      .pipe (uglifyJS ())
+      .pipe (gulp.dest ('./root/js/'));
+});
+gulp.task ('minify-html', function () {
+  gulp.src ('./root/*.html')
+      .pipe (htmlmin ({collapseWhitespace: true}))
+      .pipe (gulp.dest ('./root/'));
+});
