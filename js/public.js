0a8b8e91670791293c306fe58436a8dcfcf28aa4
OA Wu <comdan66@gmail.com>
2016-03-02 12:54:47 +0800

    Temp

--- js/public.js
+++ js/public.js
@@ -1,8 +1,29 @@
 /**
  * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2016 OA Wu Design
+ * @copyright   Copyright (c) 2015 OA Wu Design
  */
 
 $(function () {
+  var $map = $('#map');
+  var _map = null;
 
+  function initialize () {
+    _map = new google.maps.Map ($map.get (0), {
+        zoom: 11,
+        zoomControl: true,
+        scrollwheel: true,
+        scaleControl: true,
+        mapTypeControl: false,
+        navigationControl: true,
+        streetViewControl: false,
+        disableDoubleClickZoom: true,
+        center: new google.maps.LatLng (25.04, 121.55),
+      });
+    _map.mapTypes.set ('map_style', new google.maps.StyledMapType ([
+      { featureType: 'transit', stylers: [{ visibility: 'off' }] },
+      { featureType: 'poi', stylers: [{ visibility: 'off' }] }
+    ]));
+    _map.setMapTypeId ('map_style');
+  }
+  google.maps.event.addDomListener (window, 'load', initialize);
 });
