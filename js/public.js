c2012c4bb1e053a76e522992227812665c3efaf1
OA Wu <comdan66@gmail.com>
2016-07-25 23:34:44 +0800

    Tmp

--- js/public.js
+++ js/public.js
@@ -5,4 +5,20 @@
 
 $(function () {
 
+  $('label.checkbox input').change (function () {
+    $(this).prop ('disabled', true).parent ().addClass ('loading');
+    setTimeout (function () {
+      $(this).prop ('disabled', false).parent ().removeClass ('loading');
+      $(this).prop ('checked', $(this).prop ('checked'));
+    }.bind ($(this)), 1000);
+  });
+  $('label.switch input').change (function () {
+    $(this).prop ('disabled', true).parent ().addClass ('loading');
+
+    setTimeout (function () {
+      $(this).prop ('disabled', false).parent ().removeClass ('loading');
+      $(this).prop ('checked', $(this).prop ('checked'));
+    }.bind ($(this)), 1000);
+  });
+
 });
