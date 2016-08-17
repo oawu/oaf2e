56ae9e62b02ff6d3ca19d4706b7c6d5950d5ef91
OA Wu <comdan66@gmail.com>
2016-08-17 10:12:26 +0800

    Temp

--- js/public.js
+++ js/public.js
@@ -4,5 +4,28 @@
  */
 
 $(function () {
+  $('#menu_button, #cover').click (function () {
+    $('body').toggleClass ('menu_show');
+  });
+  $('.tab').each (function () {
+    var $that = $(this);
+
+    $that.find ('.titles > a').click (function () {
+      $(this).addClass ('show').siblings ().removeClass ('show');
+      $that.find ('.panels > div').eq ($(this).index ()).addClass ('show').siblings ().removeClass ('show');
+    }).first ().click ();
+  });
+
+  $('.todo').each (function () {
+    var $that = $(this);
+    $that.find ('button').click (function () {
+      var $input = $that.find ('input');
+      if (!$input.val ().length) return;
+
+      $('<span />').text ($input.val ()).appendTo ($that.find ('.list'));
+      $input.val ('');
+    });
+
+  });
 
 });
