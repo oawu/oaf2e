542fa0b765fd3574d8306b95c7099378390cb3f6
OA Wu <comdan66@gmail.com>
2017-03-20 11:13:29 +0800

    Update setting、add mixin

--- cmd/config.rb
+++ cmd/config.rb
@@ -1,40 +1,40 @@
 require 'compass/import-once/activate'
-# Require any additional compass plugins here.
 
-# Set this to the root of your project when deployed:
-
+# 網域(domain)後面的目錄
 http_path = "/oaf2e"
 
+
+# 字體目錄與網址下的字體目錄
 fonts_dir = "font"
 fonts_path = "../font"
 
+# 圖片目錄與網址下的圖片目錄
 images_dir = "img"
 images_path = "../img"
 
+# css 目錄與 scss 目錄
 css_dir = "../css"
 sass_dir = "../scss"
 
+# 其他要匯入的資源
 add_import_path = "./imports"
 additional_import_paths = ["./imports"]
 
-# images_dir = "images"
-# javascripts_dir = "javascripts"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-# output_style = :expanded
+# 選擇輸出的 css 類型，:expanded or :nested or :compact or :compressed
+  # nested     有縮排 沒壓縮
+  # expanded   沒縮排 沒壓縮
+  # compact    有換行 有壓縮(半壓縮)
+  # compressed 沒縮排 有壓縮(全壓縮)
 output_style = :compressed
+
+# 在 css 中加入註解相對應於 scss 的第幾行，false、true
+  # false     不需加入註解
+  # true      需要加入註解
+line_comments = false
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-line_comments = false
+# images_dir = "images"
+# javascripts_dir = "javascripts"
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
