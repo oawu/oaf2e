fe3d0e320011d083d92dd36d7022f4b6255bf3a0
OA Wu <comdan66@gmail.com>
2017-09-06 18:41:46 +0800

    no message

---
+++ a.php
@@ -0,0 +1,5 @@
+<?php
+
+echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
+var_dump ($_FILES);
+exit ();
--- js/public.js
+++ js/public.js
@@ -3,7 +3,34 @@
  * @copyright   Copyright (c) 2017 OA Wu Design
  * @license     http://creativecommons.org/licenses/by-nc/2.0/tw/
  */
+ 
+window.fbAsyncInit = function () { FB.init ({ appId: '151833618725768', cookie: true, xfbml: true, version: 'v2.10' }); };
+(function(d, s, id){ var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/zh_TW/sdk.js"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));
 
 $(function () {
+  var loading = {
+    $el: $('#loading'),
+    ter: [],
+    clrTer: function (str) {
+      this.ter.map (clearTimeout);
+      this.ter = [];
+    },
+    show: function (str) {
+      if (typeof str !== 'undefined') this.$el.text (str);
+      this.clrTer ();
+      this.$el.addClass ('s');
+      this.ter.push (setTimeout (function () { this.$el.addClass ('a'); }.bind (this), 100));
+    },
+    close: function () {
+      this.clrTer ();
+      this.$el.removeClass ('a');
+      this.ter.push (setTimeout (function () { this.$el.removeClass ('s'); }.bind (this), 330));
+    },
+  };
+
+  // loading.show ();
+  // setTimeout (function () {
+  //   loading.close ();
+  // }, 3000);
 
 });
