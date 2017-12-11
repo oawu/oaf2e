dc4e586f83745f2afa0c06fa0a18ff527c9c09a0
OA Wu <comdan66@gmail.com>
2017-12-11 18:29:19 +0800

    Fix code

--- cmd/libs/Minify.php
+++ cmd/libs/Minify.php
@@ -4,6 +4,7 @@
  * @author      OA Wu <comdan66@gmail.com>
  * @copyright   Copyright (c) 2017 OA Wu Design
  * @license     http://creativecommons.org/licenses/by-nc/2.0/tw/
+ * @link        https://www.ioa.tw/
  */
 
 include_once PATH_CMD_LIBS . 'Minify' . DIRECTORY_SEPARATOR . 'CSSMin.php';
--- cmd/libs/OAS3Tool.php
+++ cmd/libs/OAS3Tool.php
@@ -4,6 +4,7 @@
  * @author      OA Wu <comdan66@gmail.com>
  * @copyright   Copyright (c) 2017 OA Wu Design
  * @license     http://creativecommons.org/licenses/by-nc/2.0/tw/
+ * @link        https://www.ioa.tw/
  */
 
 class OAS3Tool {
