2cc03295c0ebcbbab27cb156fb9f7d431acb19e3
OA Wu <comdan66@gmail.com>
2018-03-30 18:29:51 +0800

    Fix bug.

--- js/public.js
+++ js/public.js
@@ -6,5 +6,16 @@
  */
  
 $(function () {
+  $.get ('api_project.json').done (function (r) {
+    var selector = 'loading';
+    $('#' + selector).html (_.template ($('#_' + selector).text ()) (r));
+  });
+
+  $('.format').each (function () {
+    var $that = $(this).attr ('data-i', 1);
+    $that.find ('.tabs > *').click (function () {
+      $that.attr ('data-i', $(this).index () + 1);
+    });
+  });
 
 });
